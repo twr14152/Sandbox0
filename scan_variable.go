916a7290be185e5e2b419ff97a1252139a59df27
twr14152 <twr14152@gmail.com>
2017-08-04 17:30:34 +0000

    using var input for computation and print operation

--- scan_variable.go
+++ scan_variable.go
@@ -12,14 +12,11 @@ func main() {
 	add := val1 + val2
 	// fmt.Printf will print values of %+v Println will not
 	fmt.Printf("%+v + %+v = %+v\n", val1, val2, add)
-        sub := val1 - val2
-        fmt.Printf("%+v - %+v = %+v\n", val1, val2, sub)
-        mult := val1 * val2
-        fmt.Printf("%+v * %+v = %+v\n", val1, val2, mult)
-        div := val1 / val2
-        fmt.Printf("%+v / %+v = %+v\n", val1, val2, div)
-
-
-
+	sub := val1 - val2
+	fmt.Printf("%+v - %+v = %+v\n", val1, val2, sub)
+	mult := val1 * val2
+	fmt.Printf("%+v * %+v = %+v\n", val1, val2, mult)
+	div := val1 / val2
+	fmt.Printf("%+v / %+v = %+v\n", val1, val2, div)
 
 }
