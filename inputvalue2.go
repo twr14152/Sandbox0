18a63511673fa24ba900fc6212ac2a447d1ee56b
Todd Riemenschneider <twr14152@gmail.com>
2017-08-04 15:49:03 -0400

    Update inputvalue2.go
    Changed %+v to %v

--- inputvalue2.go
+++ inputvalue2.go
@@ -1,22 +1,27 @@
+// Goal of this script is to mix float values with strings and to get run the computation
+
+
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Print("Enter a number: ")
-	var val1 float64
-	fmt.Scanf("%f", &val1)
-	fmt.Print("Enter a second number: ")
-	var val2 float64
-	fmt.Scanf("%f", &val2)
-	add := val1 + val2
-	// fmt.Printf will print values of %+v Println will not
-	fmt.Printf("%+v + %+v = %+v\n", val1, val2, add)
-	sub := val1 - val2
-	fmt.Printf("%+v - %+v = %+v\n", val1, val2, sub)
-	mult := val1 * val2
-	fmt.Printf("%+v * %+v = %+v\n", val1, val2, mult)
-	div := val1 / val2
-	fmt.Printf("%+v / %+v = %+v\n", val1, val2, div)
+        fmt.Print("Enter a number: ")
+        var val1 float64
+        fmt.Scanf("%f", &val1)
+        fmt.Print("Enter a second number: ")
+        var val2 float64
+        fmt.Scanf("%f", &val2)
+        add := val1 + val2
+        // fmt.Printf will print values of %v Println will not
+        fmt.Printf("%v + %v = %v\n", val1, val2, add)
+        sub := val1 - val2
+        fmt.Printf("%v - %v = %v\n", val1, val2, sub)
+        mult := val1 * val2
+        fmt.Printf("%v * %v = %v\n", val1, val2, mult)
+        div := val1 / val2
+        fmt.Printf("%v / %v = %v\n", val1, val2, div)
 
 }
+
+
