1b20ff943fc28d9b5738885d48419d12df17e5ed
twr14152 <twr14152@gmail.com>
2017-08-04 17:28:49 +0000

    variable input and operation

---
+++ scan_variable.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Enter a number: ")
+	var val1 float64
+	fmt.Scanf("%f", &val1)
+	fmt.Print("Enter a second number: ")
+	var val2 float64
+	fmt.Scanf("%f", &val2)
+	add := val1 + val2
+	// fmt.Printf will print values of %+v Println will not
+	fmt.Printf("%+v + %+v = %+v\n", val1, val2, add)
+        sub := val1 - val2
+        fmt.Printf("%+v - %+v = %+v\n", val1, val2, sub)
+        mult := val1 * val2
+        fmt.Printf("%+v * %+v = %+v\n", val1, val2, mult)
+        div := val1 / val2
+        fmt.Printf("%+v / %+v = %+v\n", val1, val2, div)
+
+
+
+
+}
