a9944105d92c9e18156415791f6528249d13de41
Todd Riemenschneider <twr14152@gmail.com>
2020-11-15 20:11:36 -0500

    Create verify.go

---
+++ in_easy_steps/ch5/verify/verify.go
@@ -0,0 +1,11 @@
+package verify
+
+import "fmt"
+
+func IsPosInt(num int) (int, error) {
+	if num < 1 {
+		err := fmt.Errorf("%v not a positive integer", num)
+		return -1, err
+	}
+	return num, nil
+}
