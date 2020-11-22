10be630562beb6c72d731c7912fe2f8f5dab70d5
Todd Riemenschneider <twr14152@gmail.com>
2020-11-22 09:44:07 -0500

    Update channel.go

--- in_easy_steps/ch11/channel.go
+++ in_easy_steps/ch11/channel.go
@@ -6,29 +6,43 @@ import (
 )
 
 func count(msg string, c chan string) {
+	fmt.Println("Channel Sender")
 	for i := 0; i < 3; i++ {
+		fmt.Println("Sending message: ")
 		c <- msg + " at " + time.Now().Format("04:05")
 		time.Sleep(1 * time.Second)
 	}
 	close(c)
+	fmt.Println("Sender closing channel")
 }
 
 func main() {
 	c := make(chan string)
 	go count("Message", c)
 	for {
+		fmt.Println("Channel Receiver - Open")
 		msg, open := <-c
 		if !open {
+			fmt.Println("Channel receiver - Closed")
 			break
 		}
 		fmt.Println(msg)
 	}
 }
-
 /*
 pi@RaspPi4:~/Coding/Go_folder/in_easy_steps/ch11 $ go run channel.go 
-Message at 26:48
-Message at 26:49
-Message at 26:50
+Channel Receiver - Open
+Channel Sender
+Sending message: 
+Message at 43:21
+Channel Receiver - Open
+Sending message: 
+Message at 43:22
+Channel Receiver - Open
+Sending message: 
+Message at 43:23
+Channel Receiver - Open
+Sender closing channel
+Channel receiver - Closed
 pi@RaspPi4:~/Coding/Go_folder/in_easy_steps/ch11 $ 
 */
