cf74347b50e5e3be997a184f25991ee9eb3a8e92
Todd Riemenschneider <twr14152@gmail.com>
2020-11-24 22:19:02 -0500

    Update find_dups.go

--- tgpl_exercises/ch1_code_samples/find_dups.go
+++ tgpl_exercises/ch1_code_samples/find_dups.go
@@ -30,4 +30,16 @@ test
  ./main < test.txt 
 2   This is a test
 3   test
+
+----
+%d de cimal integer
+%x, %o, %b integer in hexade cimal, octal, binar y
+%f, %g, %e floating-p oint number: 3.141593 3.141592653589793 3.141593e+00
+%t boole an: true or false
+%c rune (Unico de co de point)
+%s st ring
+%q quot ed str ing "abc" or rune 'c'
+%v any value in a natural for mat
+%T type of any value
+%% literal percent sig n (no operand)
 */
