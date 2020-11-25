927f8e2da6609aeb39b2ba264e32b1dce3b2adad
Todd Riemenschneider <twr14152@gmail.com>
2020-11-25 08:06:53 -0500

    Update ans_1.8.go

--- tgpl_exercises/ans_1.8.go
+++ tgpl_exercises/ans_1.8.go
@@ -2,43 +2,43 @@ package main
 
 import (
 	"fmt"
-    "io"
+	"io"
 	"net/http"
-    "strings"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
-    if strings.HasPrefix(url, "http://") {
-      fmt.Println(url)
-      resp, err := http.Get(url)
-		  if err != nil {
-			  fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-		  	os.Exit(1)
-	  	}
-		  b, err := io.Copy(os.Stdout, resp.Body)
-		  resp.Body.Close()
-	  	if err != nil {
-		  	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
+		if strings.HasPrefix(url, "http://") {
+			fmt.Println(url)
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+				os.Exit(1)
+			}
+			b, err := io.Copy(os.Stdout, resp.Body)
+			resp.Body.Close()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+				os.Exit(1)
+			}
+			fmt.Printf("%s", b)
+		} else {
+			url = "http://" + url
+			fmt.Println("This is whats getting tested: ", url)
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+				os.Exit(1)
+			}
+			b, err := io.Copy(os.Stdout, resp.Body)
+			resp.Body.Close()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+				os.Exit(1)
+			}
+			fmt.Printf("%s", b)
 		}
-		fmt.Printf("%s", b)
-    } else {
-      url = "http://" + url
-     fmt.Println("This is whats getting tested: ", url)
-      resp, err := http.Get(url)
-		  if err != nil {
-		  	fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-		  	os.Exit(1)
-	  	}
-	  	b, err := io.Copy(os.Stdout, resp.Body)
-	  	resp.Body.Close()
-	  	if err != nil {
-	  		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
 	}
-}
 }
