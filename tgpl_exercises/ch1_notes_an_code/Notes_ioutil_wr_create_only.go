e0c815e51f97a66560b40cb79f9921c55fd930ac
Todd Riemenschneider <twr14152@gmail.com>
2020-11-25 06:34:25 -0500

    Create Notes_ioutil_wr_create_only.go

---
+++ tgpl_exercises/ch1_notes_an_code/Notes_ioutil_wr_create_only.go
@@ -0,0 +1,30 @@
+//Looks like ioutil is good for reading and creating files.
+//Use os.OpenFile to append data to existing file.
+
+package main
+
+import (
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+  
+	message := []byte("This line will get overwritten")
+  message1 := []byte("Does not look like you can append.\nThis is line 2\nline 3\nEtc..\n")
+	err := ioutil.WriteFile("hello.txt", message, 0644)
+  err = ioutil.WriteFile("hello.txt", message1, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+ ./main
+ cat hello.txt 
+Does not look like you can append.
+This is line 2
+line 3
+Etc..
+ 
+*/
