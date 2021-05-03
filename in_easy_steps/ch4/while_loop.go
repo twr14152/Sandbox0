e9726da8d058dcb6d0e355133ea9b70737647d2b
Todd Riemenschneider <twr14152@gmail.com>
2021-05-03 10:13:55 -0400

    Create while_loop.go

---
+++ in_easy_steps/ch4/while_loop.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 1
+  // loop while condition is true
+	for i <= 5 {
+		fmt.Println("loop number", i)
+		i++
+	}
+}
+
+/*
+loop number 1
+loop number 2
+loop number 3
+loop number 4
+loop number 5
+*/
