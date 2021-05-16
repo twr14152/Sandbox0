90b9cb567f9a4f158107bcd9839dbdad3d1515bd
Todd Riemenschneider <twr14152@gmail.com>
2021-05-16 11:02:06 -0400

    Create iterate_array.go

---
+++ in_easy_steps/ch7/iterate_array.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var cars [4]string
+	cars[0] = "honda civic"
+	cars[1] = "ford mustang"
+	cars[2] = "chevy volt"
+	cars[3] = "toyota corolla"
+
+	coords := [2][3]int{{1, 2, 3}, {4, 5, 6}}
+
+	fmt.Println("Cars:", cars)
+	fmt.Println("Coords:", coords)
+	fmt.Println()
+	fmt.Println("Using for loop to range through the array elements:")
+	for i := range cars {
+		fmt.Print("[", cars[i], "]")
+		fmt.Print(",")
+	}
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("For loop through array using len(array):")
+	for i := 0; i < len(cars); i++ {
+		fmt.Print("[", cars[i], "]")
+		fmt.Print(",")
+	}
+	fmt.Println()
+	fmt.Println()
+	for coord := range coords {
+		fmt.Println(coords[coord])
+	}
+}
+
+/* 
+Cars: [honda civic ford mustang chevy volt toyota corolla]
+Coords: [[1 2 3] [4 5 6]]
+
+Using for loop to range through the array elements:
+[honda civic],[ford mustang],[chevy volt],[toyota corolla],
+
+For loop through array using len(array):
+[honda civic],[ford mustang],[chevy volt],[toyota corolla],
+
+[1 2 3]
+[4 5 6]
+*/
