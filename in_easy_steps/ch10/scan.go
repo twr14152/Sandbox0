a7a7d91bf1131a0451f01bf70cbf6f48713945f0
Todd Riemenschneider <twr14152@gmail.com>
2021-05-19 10:04:44 -0400

    Create scan.go

---
+++ in_easy_steps/ch10/scan.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+  "math/rand"
+  "time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+  var num int = rand.Intn(20)+1
+  var guess int = 0
+  var flag bool = true
+  fmt.Print("Guess my number its between 1-20:")
+  for flag {
+    _, err := fmt.Scan(&guess)
+      if err != nil {
+        fmt.Println(err)
+      } else if guess > num {
+        fmt.Println("Too High, try again:")
+      }else if guess < num {
+        fmt.Println("Too Low, try again:")
+      }else if guess == num {
+        fmt.Println("Correct - My number is", num)
+        flag = false
+      }
+
+  }
+}
+/*
+ ./main
+Guess my number its between 1-20:10
+Too High, try again:
+9
+Correct - My number is 9
+ 
+*/
