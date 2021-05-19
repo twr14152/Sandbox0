6120b1c1f3799782e52cfdf8b95cfbc48e8457ed
Todd Riemenschneider <twr14152@gmail.com>
2021-05-19 10:06:30 -0400

    Update scan.go

--- in_easy_steps/ch10/scan.go
+++ in_easy_steps/ch10/scan.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"fmt"
-  "math/rand"
-  "time"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-  var num int = rand.Intn(20)+1
-  var guess int = 0
-  var flag bool = true
-  fmt.Print("Guess my number its between 1-20:")
-  for flag {
-    _, err := fmt.Scan(&guess)
-      if err != nil {
-        fmt.Println(err)
-      } else if guess > num {
-        fmt.Println("Too High, try again:")
-      }else if guess < num {
-        fmt.Println("Too Low, try again:")
-      }else if guess == num {
-        fmt.Println("Correct - My number is", num)
-        flag = false
-      }
+	var num int = rand.Intn(20) + 1
+	var guess int = 0
+	var flag bool = true
+	fmt.Print("Guess my number its between 1-20:")
+	for flag {
+		_, err := fmt.Scan(&guess)
+		if err != nil {
+			fmt.Println(err)
+		} else if guess > num {
+			fmt.Println("Too High, try again:")
+		} else if guess < num {
+			fmt.Println("Too Low, try again:")
+		} else if guess == num {
+			fmt.Println("Correct - My number is", num)
+			flag = false
+		}
 
-  }
+	}
 }
 /*
  ./main
