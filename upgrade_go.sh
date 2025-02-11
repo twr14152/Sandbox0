245098dd5b23d4dd5dde5422fc6255bd3a6ec33a
Todd Riemenschneider <twr14152@gmail.com>
2025-02-11 11:07:01 -0500

    Create upgrade_go.sh
    Script to upgrade Go on Raspberry Pi

---
+++ upgrade_go.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# To download
+#wget https://go.dev/dl/go1.23.6.linux-armv6l.tar.gz
+
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.23.6.linux-armv6l.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+go version
+
