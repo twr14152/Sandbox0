be8d612ef27faf3ac0cd7506af2a8dbad76e6cca
Todd Riemenschneider <twr14152@gmail.com>
2025-02-11 11:12:07 -0500

    Update upgrade_go.sh
    tested and worked

--- upgrade_go.sh
+++ upgrade_go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # To download
-#wget https://go.dev/dl/go1.23.6.linux-armv6l.tar.gz
+wget https://go.dev/dl/go1.23.6.linux-armv6l.tar.gz
 
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.23.6.linux-armv6l.tar.gz
