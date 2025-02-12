7a1f2b48aca5711dac1cd9c04848b4c38fa4327c
Todd Riemenschneider <twr14152@gmail.com>
2025-02-12 14:58:33 -0500

    Update buffo.NewScanner_script.go

--- 2025/buffo.NewScanner_script.go
+++ 2025/buffo.NewScanner_script.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"log"
+        "bufio"
+        "fmt"
+        "log"
+        "os"
 )
 
 //Use bufio.NewScanner for line by line input
 //Use bufio.NewReader for more granular line reading and manipulation
 
 func main() {
-	fmt.Printf("Enter something>>")
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
-		if line == "quit" {
-			fmt.Println("Good bye...")
-			break
-		}
-		fmt.Print("Enter something again>>")
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+        fmt.Println("Enter a sentence and \"quit\" to end...")
+        fmt.Printf(":>>")
+        scanner := bufio.NewScanner(os.Stdin)
+        for scanner.Scan() {
+                line := scanner.Text()
+                fmt.Println(line)
+                if line == "quit" {
+                        fmt.Println("Good bye...")
+                        break
+                }
+                fmt.Print("Enter something again>>")
+        }
+        if err := scanner.Err(); err != nil {
+                log.Fatal(err)
+        }
 }
